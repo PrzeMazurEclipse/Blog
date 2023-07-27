b1f0524bbd6279878b8216b3caf85c41c80a0a19
Przemyslaw Mazurkiewicz <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-07-27 22:03:18 +0200

    Add module initial files

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YellowCard\ProductsExporter;
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'YellowCard_Blog', __DIR__);
