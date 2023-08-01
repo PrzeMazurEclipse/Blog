0de7e77dc9ffc7f8a690ad958cefb4505e6ef2b0
Przemyslaw Mazurkiewicz <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-08-01 10:34:57 +0200

    Fix collection model

--- Model/ResourceModel/Post/Collection.php
+++ Model/ResourceModel/Post/Collection.php
@@ -2,16 +2,16 @@
 
 declare(strict_types=1);
 
-namespace YallowCard\Blog\Model\ResourceModel\Post;
+namespace YellowCard\Blog\Model\ResourceModel\Post;
 
-use Magento\Cms\Model\ResourceModel\AbstractCollection;
-use Yellowcard\Blog\Model\Post;
-use YellowCard\Blog\Model\ResourceModel\Post as PostResource;
+use YellowCard\Blog\Model\Post;
+use YellowCard\Blog\Model\ResourceModel\Post as PostResourceModel;
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 
 class Collection extends AbstractCollection
 {
     protected function _construct()
     {
-        $this->_init(Post::class, PostResource::class);
+        $this->_init(Post::class, PostResourceModel::class);
     }
 }
